fabc74f4974999e66cad9ef454313963d42015a1
sandras15 <sandras15@ru.is>
2016-12-11 21:34:49 -0800

    Build script so npm can work with aws

---
+++ build.sh
@@ -0,0 +1,15 @@
+export NODE_PATH=.
+
+npm run clean
+npm run createbuild           
+npm run buildclient
+
+mv client/build build/static
+
+cp -R server build/server
+
+mkdir -p build/client/source 					#Create source folder under
+
+cp -r client/src/common build/client/source		#Copy client source over to build source
+cp run.js build 								#Copy run.js over to build folder
+cp runserver.sh build 							#Copy runserver.sh script to build folcer

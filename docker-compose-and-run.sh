a2cb768bbf70081d4fbb7ab0ef9f0b3eae7210e0
sandras15 <sandras15@ru.is>
2016-12-14 14:24:35 +0000

    Created if loop in docer-compose-and-run.yaml

--- docker-compose-and-run.sh
+++ docker-compose-and-run.sh
@@ -3,7 +3,9 @@
 set -e 							#Stops the script run if any errors occur.
 
 docker-compose down 			#Stop docker-compose
-docker rmi $(docker images -q)	#Remove docker images
+if [ -z "$docker rmi" ]; then
+  export docker rmi $(docker images -q)  #Remove docker images
+fi
 docker-compose up -d 			#Run docker-compse
 
-exit 0							##Exit status 0 if succsess, otherwise it will exit with another exitcode. 0 = Good, 1 = Bad
+exit 0							#Exit status 0 if succsess, otherwise it will exit with another exitcode. 0 = Good, 1 = Bad

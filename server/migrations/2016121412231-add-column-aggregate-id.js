33cd54aa71e2f38e44c381d779f24d14969a8192
sandras15 <sandras15@ru.is>
2016-12-14 17:52:50 +0000

    Added scripts to folder and fixed needed file for locations of scripts

--- server/migrations/2016121412231-add-column-aggregate-id.js
+++ server/migrations/2016121412231-add-column-aggregate-id.js
@@ -1,67 +1,28 @@
-exports.up = function(db, callback) {
+'use strict';
 
-  //automatic mapping, the mapping key resolves to the column
-  db.createTable( 'aggregate_id',
-  {
-      id:
-      {
-        type: 'int',
-        unsigned: true,
-        notNull: true,
-        primaryKey: true,
-        autoIncrement: true,
-        length: 10
-      },
-      aggregate_id:
-      {
-        type: 'int',s
-        unsigned: true,
-        length: 10,
-        notNull: true,
-        foreignKey: {
-          name: 'aggregate_id_fk',
-          table: 'aggregate',
-          rules: {
-            onDelete: 'CASCADE',
-            onUpdate: 'RESTRICT'
-          },
-          mapping: 'id'
-        }
-      },
-  }, callback );
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
 };
 
-exports.up = function(db, callback) {
-
-  //explicit mapping
-  db.createTable( 'aggregate_id',
-  {
-    id:
-    {
-      type: 'int',
-      unsigned: true,
-      notNull: true,
-      primaryKey: true,
-      autoIncrement: true,
-      length: 10
-    },
-    aggregate_id:
-    {
-      type: 'int',
-      unsigned: true,
-      length: 10,
-      notNull: true,
-      foreignKey: {
-        name: 'aggregate_id_fk',
-        table: 'aggregate',
-        rules: {
-          onDelete: 'CASCADE',
-          onUpdate: 'RESTRICT'
-        },
-        mapping: {
-          aggregate_id: 'id'
-        }
-      }
-    },
-  }, callback );
+exports.up = function(db,callback) {
+  db.addColumn('eventlog', 'aggregate_id'{ 
+    type: 'string'
+  }, 
+  callback);
 };
+
+
+exports._meta = {
+  "version": 1
+};
+

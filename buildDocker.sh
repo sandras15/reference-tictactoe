fd8ad9cc7a4e8e70c7ed356f94c3970d3031f0b1
sandras15 <sandras15@ru.is>
2016-12-19 02:08:06 +0000

    updated tests

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,10 +2,11 @@
 
 #echo Installing npm...       ##Write the message "Installing npm..."
 #sudo npm install -g nodemon  #Install at first
-
+rm -rf node_modules
 npm install
 
 cd client
+rm -rf node_modules
 npm install
 
 cd ..
